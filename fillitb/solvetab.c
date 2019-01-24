32cf3b0859bb73a2dc0ab7dbdde961a9058425cb
Paul-adrien LAURENT <plaurent@e1r8p21.42.fr>
2019-01-24 13:39:25 +0100

    ajout de com et petit changement

--- fillitb/solvetab.c
+++ fillitb/solvetab.c
@@ -6,20 +6,21 @@
 /*   By: plaurent <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/23 11:32:34 by plaurent          #+#    #+#             */
-/*   Updated: 2019/01/23 18:24:37 by plaurent         ###   ########.fr       */
+/*   Updated: 2019/01/24 13:37:24 by plaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-int		pose(int *str, int **map, int i, int j, int k)
+
+
+
+/* pose doit poser le tetri donner a lemplacement indiquer. */
+
+int		pose(int *str, int **map, int i, int j)
 {
 	int		n;
-	//int		m;
-	//int		l;
 
-	//m = i;
-	//l = j;
 	n = -1;
 	while (str[++n] != '\0')
 	{
@@ -35,38 +36,38 @@ int		pose(int *str, int **map, int i, int j, int k)
 	return (1);
 }
 
+
+/* remplissage doit poser des qu'il peut le str[tab[k]] tetri en gardant la
+ * position où il a poser dans str[nt] et str[nt + 1] pour aller plus vite*/
+
 int		*remplissage(int **map, int t, char **str, int *tab, int k)
 {
-	int		i;
-	int		j;
 	int		n;
 
-	n = -1;
-	i = 0;
-	j = 0;
-	while (str[tab[++n]] != '\0' && k = 1)
+	k = k + 1;
+	n = 0;
+	while (str[n] != '\0')
+		n++;
+	n = n - 1;
+	while (pose(str[tab[k]], map, (str[n][0] - 48), (str[n][1] - 48)) != 1)
 	{
-		k = 0;
-		if (i == t)
+		if (str[n][0] == (t + 47) && str[n][1] == (t + 44))
+			return (2)
+		else if (str[n][1] == (t + 47) && str[n][0] != (t + 47))
 		{
-			k = n - 1;
-			return (tab);
+			str[n][0] += 1;
+			str[n][1] = 0;
 		}
-		while (i < t && k == 0)
-		{
-			if (j = t)
-				j = 0;
-			while (j < t && k == 0)
-			{
-				if (pose(str[n], &map, i, j, 0))
-					k = 1;
-				else // else car sinon quand la piece est pas en coller haut gauche ca pose prblm
-					j++;
-			}
-			i++;
-		}
+		else if (str[n][1] != (t - 1))
+			str[n][1] += 1;
 	}
+	return (1)
 }
+
+
+/* place recois tab avec un ordre qu'il doit tester, il créé une map pour le
+ * tester, apl de la fonction remplissage en recusive, si le remplissage echoue
+ * apl de maj tab et fin de la fonction pour recommancer avec un nouveau tab*/
 
 int		*place(char **str, int t, char *tab, int k)
 {
@@ -74,15 +75,30 @@ int		*place(char **str, int t, char *tab, int k)
 	int		**map;
 
 	i = 0;
-	if (!(map = (int **)malloc(sizeof(int *) * t + 4)))
-		return (0);
-	while (i < (t + 4))
-		if (!(map[i++] = (int *)malloc(sizeof(int * (t + 4)))))
+	if (k = -1)
+	{
+		if (!(map = (int **)malloc(sizeof(int *) * t + 4)))
 			return (0);
-	tab = remplissage(map, t, str, tab, &k);
-	free(map);
-	return (tab);
+		while (i < (t + 4))
+			if (!(map[i++] = (int *)malloc(sizeof(int * (t + 4)))))
+				return (0);
+	}
+	//remplissage(map, t, str, tab, &k);
+	if (remplissage(map, t, str, tab, &k) == 2)
+	{
+		majtab(tab, k);
+		free(map);
+		return (tab);
+	}
+	else
+		return (place(map, t, str, tab, k))
 }
+
+
+/* apl la fonction place qui modifie tab, puis verifie si tab a ete remplie
+ * completement qui voudrais dire qu'on a trouver sinon on change l ordre jusqu
+ * 'a ce qu'on est tout tester et ca de facon recursive en relancant avec un
+ * tab dans place */
 
 char	backt(char **str, int t, int nt, char *tab)
 {
@@ -93,8 +109,8 @@ char	backt(char **str, int t, int nt, char *tab)
 	// ne pas oublier d'utiliser k.
 	// k = -1
 	i = 0;
-	j = nt;
-	tab = place(str, t, tab, &k);
+	j = nt - 1;
+	place(str, t, tab, &k);
 	i = 0;
 	while (i != j)
 	{
@@ -116,6 +132,10 @@ char	backt(char **str, int t, int nt, char *tab)
 	return (backt(str, t, nt, tab));
 }
 
+
+/* reception des tetri, nb de tetri et taille mini square, creation du tableau
+ * recevant l'ordre de placement des tetris. recursive qui ce fait pour
+ * augmenter de 1 le carré final quand necessaire */
 
 char	**main(char **str, int nt, int t)
 {
